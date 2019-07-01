0bc4d332bc520c02b7035ae1581fc219500783f5
Atomantium <atom@antium.com>
2019-07-01 09:20:30 -0600

    adding template

--- keyboards/dactyl_single_mcu/keymaps/default/config.h
+++ keyboards/dactyl_single_mcu/keymaps/default/config.h
@@ -16,4 +16,10 @@
 
 #pragma once
 
-// place overrides here
+// This makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when
+// you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+//#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISIVE_HOLD
+#define TAPPING_TERM 100
+//#define TAPPING_FORCE_HOLD
+#define RETRO_TAPPING
--- keyboards/dactyl_single_mcu/keymaps/default/keymap.c
+++ keyboards/dactyl_single_mcu/keymaps/default/keymap.c
@@ -15,22 +15,76 @@
  */
 #include QMK_KEYBOARD_H
 
+
+#define _QWERTY 0
+#define _COLEMAK 1
+#define _NAV 2
+#define _NUMSYM 3
+#define _NUMPAD 4
+
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
   QMKBEST = SAFE_RANGE,
   QMKURL
 };
 
+#define ALT_DOT     ALT_T(KC_DOT)
+#define ALT_X       ALT_T(KC_X)
+
+#define CTL_SLSH    CTL_T(KC_SLSH)
+#define CTL_Z       CTL_T(KC_Z)
+
+#define GUI_C       GUI_T(KC_C)
+#define GUI_COMM    GUI_T(KC_COMM)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-     RESET, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y , KC_U  , KC_I  , KC_O , KC_P  , KC_BSLS , \
-     KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                        KC_H , KC_J , KC_K  , KC_L  , KC_SCLN  , KC_QUOT , \
-     KC_LSFT , KC_Z , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N , KC_M  , KC_COMM  , KC_DOT  , KC_SLSH  , KC_RSFT , \
+     KC_NO, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y , KC_U  , KC_I  , KC_O , KC_P  , KC_BSLS , \
+     KC_CAPS , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                        KC_H , KC_J , KC_K  , KC_L  , KC_SCLN  , KC_QUOT , \
+     KC_LSFT , CTL_Z, KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N , KC_M  , KC_COMM  , KC_DOT , CTL_SLSH  , KC_RSFT , \
                       KC_LALT  , KC_LGUI,                                                   KC_RGUI , KC_RALT, \
-                                     KC_DEL, KC_BSPC,                        KC_SPC  , KC_1, \
-                                           KC_1, KC_TAB,             KC_ENT  , KC_2, \
-                                           KC_2, KC_3,               KC_4  , KC_3 \
+                                     KC_DEL, KC_BSPC,                        KC_SPC  , MO(_NAV), \
+                                           KC_TAB, KC_TAB,             KC_ENT  , KC_ENT, \
+                                           RESET, KC_NO,               KC_NO , KC_NO \
   ),
+  [_NAV] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //└────────┴────────┼────────┼────────┼────────┴────────┘                          └────────┴────────┼────────┼────────┼────────┴────────┘
+                       XXXXXXX, XXXXXXX,                                                                XXXXXXX, XXXXXXX,
+  //                  └────────┴────────┼────────┬────────┐                          ┌────────┬────────┼────────┴────────┘
+                                         _______, _______,                            _______, _______,
+  //                                    └───┬────┴───┬────┴───┐                 ┌────┴───┬────┴───┬────┘
+                                             _______, _______,                   _______, _______,
+  //                                        └───┬────┴───┬────┴───┐        ┌────┴───┬────┴───┬────┘
+                                                 RESET, _______,          _______, _______
+  //                                            └────────┴────────┘        └────────┴────────┘
+  ),
+/*
+  [_TEMPLATE] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //└────────┴────────┼────────┼────────┼────────┴────────┘                          └────────┴────────┼────────┼────────┼────────┴────────┘
+                       XXXXXXX, XXXXXXX,                                                                XXXXXXX, XXXXXXX,
+  //                  └────────┴────────┼────────┬────────┐                          ┌────────┬────────┼────────┴────────┘
+                                         _______, _______,                            _______, _______,
+  //                                    └───┬────┴───┬────┴───┐                 ┌────┴───┬────┴───┬────┘
+                                             _______, _______,                   _______, _______,
+  //                                        └───┬────┴───┬────┴───┐        ┌────┴───┬────┴───┬────┘
+                                                 _______, _______,          _______, _______
+  //                                            └────────┴────────┘        └────────┴────────┘
+  ),
+*/
+
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

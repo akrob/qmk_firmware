266315980cc73ebb19daf0abd76a9a90daeb5a86
Atomantium <atom@antium.com>
2019-08-06 18:08:00 -0600

    adding dactayl other layers

--- keyboards/dactyl_single_mcu/keymaps/default/keymap.c
+++ keyboards/dactyl_single_mcu/keymaps/default/keymap.c
@@ -38,31 +38,72 @@ enum custom_keycodes {
 #define GUI_COMM    GUI_T(KC_COMM)
 
 
+//#define T_NSTB LT(_NUMSYM, KC_TAB)
+#define T_RLET LT(_NAV, KC_ENT)
+#define T_LLNP LT(_NUMPAD, KC_DEL)
+#define T_NSDEL LT(_NUMSYM, KC_DEL)
+#define T_NPTAB LT(_NUMPAD, KC_TAB)
+#define KC_SINS LSFT(KC_INS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
      KC_NO, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y , KC_U  , KC_I  , KC_O , KC_P  , KC_BSLS , \
      KC_CAPS , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                        KC_H , KC_J , KC_K  , KC_L  , KC_SCLN  , KC_QUOT , \
      KC_LSFT , CTL_Z, KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N , KC_M  , KC_COMM  , KC_DOT , CTL_SLSH  , KC_RSFT , \
                       KC_LALT  , KC_LGUI,                                                   KC_RGUI , KC_RALT, \
-                                     KC_DEL, KC_BSPC,                        KC_SPC  , MO(_NAV), \
-                                           KC_TAB, KC_TAB,             KC_ENT  , KC_ENT, \
+                                     T_NSDEL, KC_BSPC,                        KC_SPC  , MO(_NAV), \
+                                           T_NPTAB, T_NPTAB,             KC_ENT  , KC_ENT, \
                                            RESET, KC_NO,               KC_NO , KC_NO \
   ),
   [_NAV] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                            KC_HOME, KC_PGUP,  KC_UP , KC_PGDN, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX,                            KC_END , KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     _______, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //└────────┴────────┼────────┼────────┼────────┴────────┘                          └────────┴────────┼────────┼────────┼────────┴────────┘
-                       XXXXXXX, XXXXXXX,                                                                XXXXXXX, XXXXXXX,
+                       _______, _______,                                                                _______, _______,
   //                  └────────┴────────┼────────┬────────┐                          ┌────────┬────────┼────────┴────────┘
-                                         _______, _______,                            _______, _______,
+                                         KC_DEL , _______,                            _______, _______,
   //                                    └───┬────┴───┬────┴───┐                 ┌────┴───┬────┴───┬────┘
                                              _______, _______,                   _______, _______,
   //                                        └───┬────┴───┬────┴───┐        ┌────┴───┬────┴───┬────┘
-                                                 RESET, _______,          _______, _______
+                                                  RESET , _______,          _______, _______
+  //                                            └────────┴────────┘        └────────┴────────┘
+  ),
+  [_NUMSYM] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_F11 ,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,                             KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 , KC_F10 , KC_F12 ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_GRV ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                             KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_MINS, 
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TILD, KC_EXLM,  KC_AT , KC_HASH, KC_DLR , KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+  //└────────┴────────┼────────┼────────┼────────┴────────┘                          └────────┴────────┼────────┼────────┼────────┴────────┘
+                       _______, _______,                                                                _______, _______,
+  //                  └────────┴────────┼────────┬────────┐                          ┌────────┬────────┼────────┴────────┘
+                                         KC_DEL , _______,                            _______, _______,
+  //                                    └───┬────┴───┬────┴───┐                 ┌────┴───┬────┴───┬────┘
+                                             _______, _______,                   _______, _______,
+  //                                        └───┬────┴───┬────┴───┐        ┌────┴───┬────┴───┬────┘
+                                                  RESET , _______,          _______, _______
+  //                                            └────────┴────────┘        └────────┴────────┘
+  ),
+  [_NUMPAD] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX, XXXXXXX, XXXXXXX,  KC_UP ,  KC_F5 , XXXXXXX,                            KC_ASTR,  KC_7  ,  KC_8  ,  KC_9  , KC_PLUS, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT ,                            KC_SLSH,  KC_4  ,  KC_5  ,  KC_6  , KC_MINS, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, XXXXXXX, XXXXXXX, KC_BSPC, KC_SINS, KC_SPC ,                            KC_EQL ,  KC_1  ,  KC_2  ,  KC_3  , KC_DOT , _______,
+  //└────────┴────────┼────────┼────────┼────────┴────────┘                          └────────┴────────┼────────┼────────┼────────┴────────┘
+                       _______, _______,                                                                _______, _______,
+  //                  └────────┴────────┼────────┬────────┐                          ┌────────┬────────┼────────┴────────┘
+                                         _______, _______,                             KC_0  , _______,
+  //                                    └───┬────┴───┬────┴───┐                 ┌────┴───┬────┴───┬────┘
+                                             _______, _______,                   _______, _______,
+  //                                        └───┬────┴───┬────┴───┐        ┌────┴───┬────┴───┬────┘
+                                                  RESET , _______,          _______, _______
   //                                            └────────┴────────┘        └────────┴────────┘
   ),
 /*
@@ -72,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //└────────┴────────┼────────┼────────┼────────┴────────┘                          └────────┴────────┼────────┼────────┼────────┴────────┘
-                       XXXXXXX, XXXXXXX,                                                                XXXXXXX, XXXXXXX,
+                       _______, _______,                                                                _______, _______,
   //                  └────────┴────────┼────────┬────────┐                          ┌────────┬────────┼────────┴────────┘
                                          _______, _______,                            _______, _______,
   //                                    └───┬────┴───┬────┴───┐                 ┌────┴───┬────┴───┬────┘

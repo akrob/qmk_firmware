397f6bd1ce2047ca201d16b652d1d3ccc23c91f0
Atomantium <atom@antium.com>
2019-12-26 21:48:47 -0700

    adding tapping toggle config

--- keyboards/handwired/dactyl_single_mcu/keymaps/default/config.h
+++ keyboards/handwired/dactyl_single_mcu/keymaps/default/config.h
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#define TAPPING_TOGGLE 2
 // This makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when
 // you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 #define IGNORE_MOD_TAP_INTERRUPT

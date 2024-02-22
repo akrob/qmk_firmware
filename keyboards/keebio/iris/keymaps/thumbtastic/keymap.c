12b83edcc1f1da1f3ed8a0b2a3dccc9cd0bb9b7b
aroberts6 <adam_roberts@intuit.com>
2024-02-21 20:54:05 -0700

    updating to use latest reset

--- keyboards/keebio/iris/keymaps/thumbtastic/keymap.c
+++ keyboards/keebio/iris/keymaps/thumbtastic/keymap.c
@@ -79,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DUAL] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      RESET , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  OSTOG  ,                            XXXXXXX, XXXXXXX, XXXXXXX,BL_DOWN,  BL_UP , XXXXXXX,
+     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  OSTOG  ,                            XXXXXXX, XXXXXXX, XXXXXXX,BL_DOWN,  BL_UP , XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      RESET , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, XXXXXXX,
+     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,                             KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 , KC_F10 , XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤

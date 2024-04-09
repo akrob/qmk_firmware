b5b536a50db47ac3cb69ac406ad68a0e5bdee337
aroberts6 <adam_roberts@intuit.com>
2024-04-09 09:32:47 -0600

    tuning tap delay for space

--- keyboards/keebio/iris/keymaps/dualthumb/keymap.c
+++ keyboards/keebio/iris/keymaps/dualthumb/keymap.c
@@ -8,7 +8,7 @@
 
 // Used for Custom Tap/Hold for layers
 #define TAB_DELAY 150
-#define SPACE_DELAY 225
+#define SPACE_DELAY 175
 static uint16_t key_timer;
 #define RESET_DELAY 5000
 static uint16_t reset_timer;

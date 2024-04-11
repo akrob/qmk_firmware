0963252ccf2573235a9c02abb8b6ee308372e596
aroberts6 <adam_roberts@intuit.com>
2024-04-10 23:54:55 -0600

    cleaning up unused vars

--- keyboards/keebio/iris/keymaps/simplifying/keymap.c
+++ keyboards/keebio/iris/keymaps/simplifying/keymap.c
@@ -7,9 +7,6 @@
 #define _DUAL 3
 
 // Used for Custom Tap/Hold for layers
-#define TAB_DELAY 150
-#define SPACE_DELAY 175
-static uint16_t key_timer;
 #define RESET_DELAY 5000
 static uint16_t reset_timer;
 bool rlocked = false;

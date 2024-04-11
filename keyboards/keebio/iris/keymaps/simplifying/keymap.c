73ad982c85e7e8f0b00b6e2e6514af837bbe0f3b
aroberts6 <adam_roberts@intuit.com>
2024-04-11 00:47:45 -0600

    tigher!

--- keyboards/keebio/iris/keymaps/simplifying/keymap.c
+++ keyboards/keebio/iris/keymaps/simplifying/keymap.c
@@ -135,7 +135,7 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case RGHT_SP:
-            return 85;
+            return 75;
         default:
             return QUICK_TAP_TERM;
     }
